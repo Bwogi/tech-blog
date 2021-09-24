44545cdaf030e5c4accbe9906b37282740770ec9
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-09-24 09:45:27 -0700

    some work on the front-end

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Blog, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  Blog.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
+      res.render("dashboard", { blogs, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", (req, res) => {
+  Blog.findByPk(req.params.id, {
+    attributes: ["Id", "blog_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (dbBlogData) {
+        const blog = dbBlogData.get({ plain: true });
+
+        res.render("edit-blog", {
+          blog,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
