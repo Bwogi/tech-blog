769065f118f2f9f924c175142e477f152087d678
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-09-24 10:11:01 -0700

    routes for login and blog

---
+++ controllers/api/blog-routes.js
@@ -0,0 +1,121 @@
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { Blog, User, Comment } = require("../../models");
+
+router.get("/", (req, res) => {
+  Blog.findAll({
+    attributes: ["id", "title", "blog_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => res.json(dbBlogData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Blog.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "blog_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Blog.create({
+    title: req.body.title,
+    blog_text: req.body.blog_text,
+    user_id: req.session.user_id,
+  })
+    .then((dbBlogData) => res.json(dbBlogData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Blog.update(
+    {
+      title: req.body.title,
+      blog_text: req.body.blog_text,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with thisid" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Blog.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with thisid" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
